b5efe3b1ff5400fc55213b53c1e20a545af62df3
Nika Horvat <162973378+HorvatNika@users.noreply.github.com>
2024-11-15 15:47:20 +0100

    pocetke_pt3

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,9 @@
 <template>
     <div class="home">
       <div class="home-title">home</div>
+      <div class="home-text">
+        Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+      </div>
       <div class="top-background"></div>
       <div class="middle-line"></div>
       <div class="bottom-background"></div>
@@ -27,11 +30,24 @@
     font-family: 'ChunkyRetro', sans-serif;
     font-size: 17rem;
     color: #FFFEF9;
-    text-align: left; 
+    text-align: left;
     position: absolute;
     top: 1%;
-    left: 15%; 
+    left: 15%;
     width: 100%;
+    z-index: 2;
+    opacity: 60%;
+  }
+  
+  .home-text {
+    font-family: 'CenturyGothic', sans-serif; 
+    font-size: 1rem; 
+    color: #FFFEF9;
+    text-align: left;
+    margin-top: 11%; 
+    position: absolute;
+    left: 15%; 
+    width: 50%; 
     z-index: 2;
   }
   
--- src/views/Profile.vue
+++ src/views/Profile.vue
@@ -1,6 +1,9 @@
 <template>
     <div class="profile">
       <div class="profile-title">profile</div>
+      <div class="profile-text">
+        Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+      </div>
       <div class="top-background"></div>
       <div class="middle-line"></div>
       <div class="bottom-background"></div>
@@ -32,6 +35,19 @@
     top: 1%;
     left: 15%;
     width: 100%;
+    z-index: 2;
+    opacity: 60%;
+  }
+  
+  .profile-text {
+    font-family: 'CenturyGothic', sans-serif;
+    font-size: 1rem;
+    color: #FFFEF9;
+    text-align: left;
+    margin-top: 11%; 
+    position: absolute;
+    left: 15%; 
+    width: 50%; 
     z-index: 2;
   }
   
